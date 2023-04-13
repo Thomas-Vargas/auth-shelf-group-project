81e94573dc23604fa0215f7ec8741c4c31e2969b
Ia Vue <iavue00@gmail.com>
2023-04-13 13:04:13 -0500

    completed get route

---
+++ src/components/ShelfList/ShelfList.js
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+function ShelfList() {
+
+    const shelf = useSelector(store => store.shelfReducer);
+    console.log(shelf);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch({type: 'FETCH_SHELF'})
+    }, [])
+
+    return(
+        <ul>
+            {shelf.length ? 
+                shelf.map(item => (
+                    <li>
+                        {item.description}
+                        <img src={item.image_url}/>
+                    </li>
+                ))
+             : <li>Page loading...</li>
+            }
+        </ul>
+    )
+}
+
+export default ShelfList;
